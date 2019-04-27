5ad6b6b8ba04c4afa74dfcedf61a7df3008bd6d8
onlycjs <onlycjs@naver.com>
2019-04-27 19:24:25 +0900

    네이버 파싱 수업

--- index.js
+++ index.js
@@ -1,16 +1,65 @@
-let http = require('http');
-let fs = require('fs');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const request = require('request');
+const cheerio = require('cheerio');
+const bodyparser = require('body-parser');
 
-let server = http.createServer( function(req, res) {
-    res.setHeader('Content-Type', 'text.html');
-    res.statusCode = 200;
+let app = express();
 
-    let file = fs.createReadStream("./index.html");
-    file.on("open", function(){
-        file.pipe(res);
+app.set('port', 12000);
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.json()); //미들웨어로 바디파서를 사용함. jsp에서 포스트로 넘어오는걸 자동으로 리퀘스트바디에 json형식으로 넣어주는것
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render('main', { msg: 'Welcome To Express4' });
+});
+
+app.get('/top20', function (req, res) {
+
+    request("https://www.naver.com", function (err, response, body) {
+        let list = [];
+        $ = cheerio.load(body);
+
+        let top20 = $(".ah_roll_area > .ah_l > li > a > .ah_k");
+
+        for (let i = 0; i < top20.length; i++) {
+            let msg = $(top20[i]).text();
+            list.push(msg);
+        }
+
+        res.render('top', { msg: '네이버 실시간 급상승 검색어', list: list });
     });
 });
 
-server.listen(12000, function(){
-    console.log("서버가 12000번 포트에서 실행중입니다.");
+app.get('/search', function (req, res) {
+    res.render('search', {});
+});
+
+app.post('/search', function (req, res) {
+    let word = req.body.word;
+    let url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + word;
+    request(url, function (err, response, body) {
+        let list = [];
+        $ = cheerio.load(body);
+
+        let result = $(".sp_website .type01 > li dt > a:first-child");
+
+        for (let i = 0; i < result.length; i++) {
+            let msg = $(result[i]).text();
+            list.push(msg);
+        }
+
+        res.render('search', { msg: '검색 결과', list: list });
+    });
+});
+
+
+let server = http.createServer(app);
+server.listen(app.get('port'), function () {
+    console.log(`Express 엔진이 ${app.get('port')}에서 실행중`);
 });
--- req.js
+++ req.js
@@ -6,7 +6,7 @@ const options = {
     url: 'http://y-y.hs.kr/lunch.view?date=20190429',
     headers: {
         'User-Agent':'Mozilla/5.0'
-    },
+    }
 }
 request(options,function(err, res, body){ //첫번째는 주소 두번째는 받아올거
     //console.log(err); //null 이면 에러없는것 true면 에러가 있는것
